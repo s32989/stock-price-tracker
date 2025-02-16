edfed80cd6eead210f0dd3a491f0aa7ba07baff0
s32989 <s32989@gmail.com>
2025-02-15 22:36:42 -0500

    setup up db and added to dependencies for app

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ WORKDIR /app
 
 COPY . /app
 
-RUN pip install flask
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
 
 EXPOSE 5000
 
--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="db",
+        database="stock_tracker",
+        user="user",
+        password="password"
+    )
+    return conn
+
 @app.route('/')
 def home():
-    return "Hello Woyild - Curly"
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT version();')
+    version = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f"Connected to PostgreSQL: {version[0]}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
